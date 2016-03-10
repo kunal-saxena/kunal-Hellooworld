9f5e4093e1a0065db739f69a4f7c0cd3bf4f8bfe
kunal-saxena <kunal.saxena@hp.com>
2016-03-10 13:18:55 +0530

    Update HelloServlet.java
    Fixing issue

--- src/main/java/org/hp/samples/HelloServlet.java
+++ src/main/java/org/hp/samples/HelloServlet.java
@@ -32,8 +32,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private TextField input;
-	private TextField output;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/plain");
